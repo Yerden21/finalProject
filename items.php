5c3e042ac4f511b29fc45368d27d5411947067a2
Yerden21 <170107144@stu.sdu.edu.kz>
2018-12-23 15:12:16 +0600

    to show all the news sorting by janr

---
+++ items.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/items.css">
+    <script src="js/items.js" defer></script>
+    <title>Last News</title>
+</head>
+<body>
+    <?php
+        include_once 'header.php';
+    ?>
+    <div class="body">
+        <div class="side-bar"></div>
+        <div class="main-info"></div>
+    </div>
+    <?php
+        include_once 'footer.php';
+    ?>
+</body>
+</html>
