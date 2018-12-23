117377364d2be48baffc4b5353e27b8672fcf95d
Yerden21 <170107144@stu.sdu.edu.kz>
2018-12-23 15:11:38 +0600

    main page

---
+++ index.php
@@ -0,0 +1,97 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="js/index.js" defer></script>
+    <title>Welcome to news portal</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+	<?php
+		include_once 'header.php';
+	?>
+
+    <div class="main-content">
+        <div class="parent">
+            <div class="side-column">
+                <div class="item">
+                    <img src="images/news1.jpg" alt="">
+                    <div class="info">
+                        <a><h3></h3></a>
+                        <a href="items.php">Kak na eto otreagirovali</a>
+                    </div>
+                </div>
+            </div>
+            <div class="side-column">
+                <div class="parent-grid">
+                    <div class="ind-block">
+                        <div class="item">
+                            <img src="images/news1.jpg" alt="">
+                            <div class="info">
+                                <a href="details.php?num=0"><h3></h3></a>
+                                <a href="items.php">Kak na eto otreagirovali</a>
+                            </div>                   
+                        </div>
+                    </div>
+                    <div class="ind-block">
+                        <div class="item">
+                            <img src="images/news1.jpg" alt="">
+                            <div class="info">
+                                <a href="details.php?num=0"><h3></h3></a>
+                                <a href="items.php">Kak na eto otreagirovali</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ind-block">
+                        <div class="item">
+                            <img src="images/news1.jpg" alt="">
+                            <div class="info">
+                                <a href="details.php?num=0"><h3></h3></a>
+                                <a href="items.html">Kak na eto otreagirovali</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ind-block">
+                        <div class="item">
+                            <img src="images/news1.jpg" alt="">
+                            <div class="info">
+                                <a href="details.php?num=0"><h3></h3></a>
+                                <a href="items.html">Kak na eto otreagirovali</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br><br>
+        <div class="flex-list">
+            <div class="flex-list-item">
+                <img src="images/news1.jpg" alt="">
+                <h1>asdf</h1>
+                <p>asjdfhjkasdhfjk</p>
+            </div>
+            <div class="flex-list-item">
+                <img src="images/news1.jpg" alt="">
+                <h1>afsd</h1>
+                <p>asjdfhjkasdhfjasjkdhfjkashdjkfj
+                    jashdfjkhjksdhfjhasjkdfhkjasdhfk
+                    asdhjkfhsdj
+                    asdhjfkhaskdjfhjkasdk</p>
+            </div>
+            <div class="flex-list-item">
+                <img src="images/news1.jpg" alt="">
+                <h1>asdf</h1>
+                <p>asjdfhjkasdhfjk</p>
+            </div>
+        </div>
+	</div>
+	<?php
+		include_once 'footer.php';
+	?>
+</body>
+</html>
+
+
+
